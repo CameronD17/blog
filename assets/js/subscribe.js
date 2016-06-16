680ae6d44a7a059368e4bd22a25f7634977f1efc
CameronD17 <camerondoyle@live.co.uk>
2016-06-16 17:50:11 +0100

    Fixed modal pop-up to limit to only 1 instance at a time.

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,4 +1,16 @@
-$('.open-subscribe-popup').click(function() {
-    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us13.list-manage.com","uuid":"1f1ccfe804d7833ca1f07c968","lid":"519660a598"}) })
-    document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+var isClicked = false;
+
+$(document).on('click', '.mc-closeModal', function() {
+    isClicked = false;
 });
+
+$('.open-subscribe-popup').click(function() {
+    if(!isClicked) {
+        require(["mojo/signup-forms/Loader"], function(L) { 
+            L.start({"baseUrl":"mc.us13.list-manage.com","uuid":"1f1ccfe804d7833ca1f07c968","lid":"519660a598"}) 
+        })
+        document.cookie = "MCEvilPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+        isClicked = true;
+    }
+});
+
