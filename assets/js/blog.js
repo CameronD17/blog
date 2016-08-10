12d8bc33cff5fd8512bdb338cf4d06ec60ffd0bb
CameronD17 <camerondoyle@live.co.uk>
2016-08-10 15:41:15 +0100

    Added option to filter by date

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -12,59 +12,82 @@ $('.read-more').click(function() {
     }
 });
 
+
+/* Switch between category and date filter */
+$('.filter-toggle').click(function() {
+    if ($('#category-list').hasClass('filter-hidden')) {        // Switch to categories
+        $('#category-list').removeClass('filter-hidden');
+        $('#date-list').addClass('filter-hidden');
+    } else {                                                    // Switch to dates
+        $('#date-list').removeClass('filter-hidden');
+        $('#category-list').addClass('filter-hidden');
+    }
+    toggleAllPosts('on');
+    lockFilterPane();
+});
+
 /* Lock category pane to window (C) Cameron Doyle */ 
 $(window).scroll(function() {
-    lockCategoryPane();
+    lockFilterPane();
 });
 
 $(window).resize(function() {
-    lockCategoryPane();
+    lockFilterPane();
 });
 
-function lockCategoryPane(){
-    if ((($(window).height()) > $('#category-list').height() + 100) && (($('#category-list').height() + 130) < $('#blog-archive').height()) && document.documentElement.clientWidth > 1040) {
+function lockFilterPane(){
+    var filterList = $('#category-list');
+    if ($('#category-list').hasClass('filter-hidden')) {
+        filterList = $('#date-list');
+    }
+    
+    if ((($(window).height()) > filterList.height() + 100) && ((filterList.height() + 130) < $('#blog-archive').height()) && document.documentElement.clientWidth > 1040) {
         var archiveTop          = $('#blog-archive').offset().top;
         var archiveBottom       = archiveTop + $('#blog-archive').height();
-        var categoryListBottom  = $(window).scrollTop() + $('#category-list').height(); 
+        var categoryListBottom  = $(window).scrollTop() + filterList.height(); 
 
         if (($(window).scrollTop() >= archiveTop) && (categoryListBottom <= archiveBottom)) {   // Enter Archive
-            $("#category-list").removeClass("category-list-bottom");
-            $("#category-list").addClass("category-list-fixed");
+            filterList.removeClass("list-bottom");
+            filterList.addClass("list-fixed");
         } else if (categoryListBottom > archiveBottom){                                         // Exit Below Archive
-            $("#category-list").removeClass("category-list-fixed");
-            $("#category-list").addClass("category-list-bottom");
+            filterList.removeClass("list-fixed");
+            filterList.addClass("list-bottom");
         } else {                                                                                // Exit Above Archive
-            $("#category-list").removeClass("category-list-bottom");
-            $("#category-list").removeClass("category-list-fixed");
+            filterList.removeClass("list-bottom");
+            filterList.removeClass("list-fixed");
         }
     } else {                                                                                    // Exit Above Archive
-        $("#category-list").removeClass("category-list-bottom");
-        $("#category-list").removeClass("category-list-fixed");
+        filterList.removeClass("list-bottom");
+        filterList.removeClass("list-fixed");
     }
 }
 
 /* Toggle blog categories on/off (C) Cameron Doyle */
 
-// Click category in archive (category list)
+// Click filter in archive list
 $('.category').click(function() {
-    toggleCategory($(this));
+    toggleFilter($(this));
+});
+
+$('.date').click(function() {
+    toggleFilter($(this));
+});
+
+// Click Select All or Clear All (category list)
+$('.all-posts').click(function() {
+    if($(this).hasClass('selectall')) {
+        toggleAllPosts('on');
+    }
+    else {
+        toggleAllPosts('off');
+    }
+    scrollToTop();
 });
 
 // Click category on archive (not category list)
 $('.archive-filter').click(function() {
     var category = $(this).attr('class').split(' ')[1];
     toggleSingleCategory(category);
-});
-
-// Click Select All or Clear All (category list)
-$('.all-categories').click(function() {
-    if($(this).hasClass('selectall')) {
-        toggleAllCategories('on');
-    }
-    else {
-        toggleAllCategories('off');
-    }
-    scrollToTop();
 });
 
 // Click category on external page (e.g. blog post)
@@ -90,28 +113,47 @@ function applyToggles() {
         if (!$(this).hasClass('hidden')) {
             $(this).addClass('hidden');
         }
-    });    
-    
-    // Loop through the blog posts, and un-hide any blog posts that match any selected categories
-    var none = true;
-    $('.category').each(function() {
-        var category = $(this).attr('id');
-        if ($(this).hasClass('selected')) {
-            $('.archive-excerpt').each(function() {
-                if ($(this).find('tr>td>span>a').hasClass('#'+category)) {
-                    if ($(this).hasClass('hidden')) {
-                        $(this).removeClass('hidden');
-                        none = false;
-                    }
-                }
-            });
-        }
     });
+    
+    
+    // Loop through the blog posts, and un-hide any blog posts that match any selected filter
+    var filterList = $('#category-list');
+    if ($('#category-list').hasClass('filter-hidden')) {
+        filterList = $('#date-list');
+        $('.date').each(function() {
+            var date = $(this).attr('id');
+            if ($(this).hasClass('selected')) {
+                $('.archive-excerpt').each(function() {
+                    if ($(this).hasClass('#'+date)) {
+                        if ($(this).hasClass('hidden')) {
+                            $(this).removeClass('hidden');
+                            none = false;
+                        }
+                    }
+                });
+            }
+        });
+    } else {
+        var none = true;
+        $('.category').each(function() {
+            var category = $(this).attr('id');
+            if ($(this).hasClass('selected')) {
+                $('.archive-excerpt').each(function() {
+                    if ($(this).find('tr>td>span>a').hasClass('#'+category)) {
+                        if ($(this).hasClass('hidden')) {
+                            $(this).removeClass('hidden');
+                            none = false;
+                        }
+                    }
+                });
+            }
+        });
+    }
     
     // If no blog posts match the selected categories, show the "No Posts Found" default post
     if (none) {
-        $("#category-list").removeClass("category-list-bottom");
-        $("#category-list").removeClass("category-list-fixed");
+        filterList.removeClass("list-bottom");
+        filterList.removeClass("list-fixed");
         if($("#no-blog-posts").hasClass("hidden")) {
             $("#no-blog-posts").removeClass("hidden");
         }
@@ -122,43 +164,47 @@ function applyToggles() {
     scrollToTop();
 }
 
-function toggleCategory(categoryFilter) {
+function toggleFilter(filter) {
     var allSelected = true;
     var noneSelected = true;
+    var filterList = $('.category');
+    if ($('#category-list').hasClass('filter-hidden')) {
+        filterList = $('.date');
+    }
     
     // Loop through each category and check it's status
-    $('.category').each(function() {
+    filterList.each(function() {
         // If any are not selected, then we can't be clearing + selecting a single category
         if (!$(this).hasClass('selected')) {
             allSelected = false;
         }
         
         // If any are selected, then we can't be selecting all unless ONLY the current category is selected
-        if ($(this).hasClass('selected') && categoryFilter.attr('id') != $(this).attr('id')) {
+        if ($(this).hasClass('selected') && filter.attr('id') != $(this).attr('id')) {
             noneSelected = false;
         }
     });
     
     // If the current category is not selected, and neither is any other category, we only want to select one category, not all of them
-    if (!categoryFilter.hasClass('selected') && noneSelected) {
+    if (!filter.hasClass('selected') && noneSelected) {
         noneSelected = false;
     }
     
     if (allSelected) {
-        toggleAllCategories('off');
-        categoryFilter.addClass('selected');
-        categoryFilter.find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
+        toggleAllPosts('off');
+        filter.addClass('selected');
+        filter.find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
     } 
     else if (noneSelected) {
-        toggleAllCategories('on');
+        toggleAllPosts('on');
     }    
     else {
-        if (categoryFilter.hasClass('selected')) {
-            categoryFilter.removeClass('selected');
-            categoryFilter.find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
+        if (filter.hasClass('selected')) {
+            filter.removeClass('selected');
+            filter.find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
         } else {
-            categoryFilter.addClass('selected');
-            categoryFilter.find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
+            filter.addClass('selected');
+            filter.find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
         }
     }
     
@@ -166,8 +212,8 @@ function toggleCategory(categoryFilter) {
 }
 
 function toggleSingleCategory(categoryFilter) {
-    $("#category-list").removeClass("category-list-bottom");
-    $("#category-list").removeClass("category-list-fixed");
+    $("#category-list").removeClass("list-bottom");
+    $("#category-list").removeClass("list-fixed");
     
     $('.category').each(function() {
         if ($(this).hasClass('selected')) {
@@ -184,9 +230,21 @@ function toggleSingleCategory(categoryFilter) {
     applyToggles();
 }
 
-function toggleAllCategories(toggle) {
+function toggleAllPosts(toggle) {
+    var filterList = $('#category-list');
+    if ($('#category-list').hasClass('filter-hidden')) {
+        filterList = $('#date-list');
+    }
+    
     if (toggle == 'on') {
         $('.category').each(function() {
+            if (!$(this).hasClass('selected')) {
+                $(this).addClass('selected');
+                $(this).find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
+            }
+        });
+        
+        $('.date').each(function() {
             if (!$(this).hasClass('selected')) {
                 $(this).addClass('selected');
                 $(this).find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
@@ -203,9 +261,16 @@ function toggleAllCategories(toggle) {
             $("#no-blog-posts").addClass("hidden");  
         }
     } else if (toggle == 'off') {
-        $("#category-list").removeClass("category-list-bottom");
-        $("#category-list").removeClass("category-list-fixed");
+        filterList.removeClass("list-bottom");
+        filterList.removeClass("list-fixed");
         $('.category').each(function() {
+            if ($(this).hasClass('selected')) {
+                $(this).removeClass('selected');
+                $(this).find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
+            }
+        });
+        
+        $('.date').each(function() {
             if ($(this).hasClass('selected')) {
                 $(this).removeClass('selected');
                 $(this).find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
@@ -222,5 +287,4 @@ function toggleAllCategories(toggle) {
             $("#no-blog-posts").removeClass("hidden");
         }
     }    
-    $(".tag-dropdown-caption").text("Select Category");
 }
